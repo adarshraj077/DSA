3b2f2eedee7fc50b22daf72ec1e87a0d43a490d6
Adarsh Raj <spratima46154@gmail.com>
2026-09-11 23:59:30 +0530

    Unique 3-Digit Even Numbers

---
+++ Data-Structures-and-Algorithms/ Unique 3-Digit Even Numbers/solutions.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int n=digits.size();
+        int count =0;
+        vector<int>visited(999,0);
+
+        for(int i=0;i<n;i++){
+            if(digits[i]==0){continue;}
+            for(int j=0;j<n;j++){
+              if(i==j){continue;}
+              for(int k=0;k<n;k++){
+                if(k==i ||k==j || digits[k]%2 !=0){continue;}
+                int val=digits[i]*100+digits[j]*10+digits[k];
+                if(!visited[val]){
+                    visited[val]++;
+                    count++;
+                }
+              }
+            }
+        }
+
+        return count;
+
+
+    }
+};
