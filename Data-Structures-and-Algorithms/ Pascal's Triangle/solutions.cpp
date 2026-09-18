2cc6710df8bbdf40cab975b76d25b5806275612c
Adarsh Raj <spratima46154@gmail.com>
2026-09-18 23:45:55 +0530

    Pascal's Triangle

---
+++ Data-Structures-and-Algorithms/ Pascal's Triangle/solutions.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    vector<vector<int>> generate(int numRows) {
+        if(numRows==0) return {};
+        if(numRows==1){
+            return {{1}};
+        }
+ vector<vector<int>> prev=generate(numRows-1);
+ vector<int> newRow(numRows,1);
+
+ for(int i=1;i<numRows-1;i++){
+    newRow[i]=prev.back()[i-1]+prev.back()[i];
+
+ }
+
+ prev.push_back(newRow);
+ return prev;
+
+
+
+
+
+    }
+};
