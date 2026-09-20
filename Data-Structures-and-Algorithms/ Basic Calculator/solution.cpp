7cd8c1332def2041f2579ef1ca83c2dd91386ddf
Adarsh Raj <spratima46154@gmail.com>
2026-09-20 23:18:40 +0530

    Basic Calculator

---
+++ Data-Structures-and-Algorithms/ Basic Calculator/solution.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int>st;
+        long long curr=0;
+        int res=0;
+        int sign=1;
+
+        for(char c:s){
+          if(isdigit(c)){
+            curr=curr*10+(c-'0');
+          }
+          else if(c=='+'){
+            res+=curr*sign;
+            sign=1;
+            curr=0;
+          }else if(c=='-'){
+            res+=curr*sign;
+            sign =-1;
+            curr=0;
+
+          }else if(c=='('){
+             st.push(res);
+             st.push(sign);
+             res=0;
+             sign=1;
+
+          }else if(c==')'){
+            res+=curr*sign;
+            res*=st.top();
+            st.pop();
+            res+=st.top();
+              st.pop(); 
+              curr=0;
+          }
+
+
+        }
+
+        res+=curr*sign;
+
+        return res;
+
+    }
+};
